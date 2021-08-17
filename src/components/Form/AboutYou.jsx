d1b5b51f051c6b1e68ec6edc79e962777fa3c5fb
Alexandr Fisenko <80264183+Alfi2490@users.noreply.github.com>
2021-08-17 13:43:18 +0300

    design

---
+++ src/components/Form/AboutYou.jsx
@@ -0,0 +1,45 @@
+import FormGroupeBottom from "./FormGroupeBottom";
+
+function AboutYou(props) {
+
+    const formik = props.formik;
+
+    let component = [];
+
+    if (props.AboutYou) {
+        component = <div className="AboutYou">
+            <div className="FormGroupe">
+                <label htmlFor="name">Name {props.star}</label>
+                <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} className="Input" />
+            </div>
+            <div className="FormGroupe">
+                <label htmlFor="tel">Telephone</label>
+                <input type="text" name="tel" id="tel" onChange={formik.handleChange} value={formik.values.tel} placeholder="+__ _ _______" className="Input" />
+            </div>
+        </div>
+    };
+
+    if (props.AboutAJorney) {
+        component = <div className="AboutYou">
+            <div className="FormGroupe">
+                <label htmlFor="place">When and where you wan to go? {props.star}</label>
+                <input type="text" name="place" id="place" onChange={formik.handleChange} value={formik.values.place} placeholder="Continent, country of city" className="Input" />
+            </div>
+            <div className="FormGroupe">
+                <label htmlFor="date">When?</label>
+                <input type="text" name="date" id="date" onChange={formik.handleChange} value={formik.values.date} placeholder="Date or time interval" className="Input" />
+            </div>   
+        </div>
+    };
+
+    if (props.Bottom) {
+        component = <div className="AboutYou">
+        <FormGroupeBottom ConnetionType formik={formik} />     
+        <FormGroupeBottom WhereDoYouKnowAboutUs formik={formik} />
+    </div>
+    }
+
+    return component;
+}
+
+export default AboutYou;
---
+++ src/components/Form/FormGroupeBottom.jsx
@@ -0,0 +1,54 @@
+function FormGroupeBottom(props) {
+
+    const formik = props.formik;
+
+    let component =[];
+
+    if (props.ConnetionType) {
+        component = <div className="FormGroupeBottom">
+            <p className="FormPart Bottom">Connection type</p>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="viber">Viber</label>
+                    <input type="checkbox" name="viber" id="viber" onChange={formik.handleChange} value={formik.values.viber} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="whatsapp">Whatsapp</label>
+                    <input type="checkbox" name="whatsapp" id="whatsapp" onChange={formik.handleChange} value={formik.values.whatsapp} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="mail">mail</label>
+                    <input type="checkbox" name="mail" id="mail" onChange={formik.handleChange} value={formik.values.mail} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="phone">phone</label>
+                    <input type="checkbox" name="phone" id="phone" onChange={formik.handleChange} value={formik.values.phone} className="CheckBox" />
+                </div>            
+        </div>  
+    }
+    
+    if (props.WhereDoYouKnowAboutUs) {
+        component =  <div className="FormGroupeBottom">
+            <p className="FormPart Bottom">Where do you know about us?</p>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="internet">internet</label>
+                    <input type="checkbox" name="internet" id="internet" onChange={formik.handleChange} value={formik.values.internet} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="friends">friends</label>
+                    <input type="checkbox" name="friends" id="friends" onChange={formik.handleChange} value={formik.values.friends} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="network">network</label>
+                    <input type="checkbox" name="network" id="network" onChange={formik.handleChange} value={formik.values.network} className="CheckBox" />
+                </div>
+                <div className="CheckboxGroupe">
+                    <label htmlFor="already">already</label>
+                    <input type="checkbox" name="alreadyalready" id="phone" onChange={formik.handleChange} value={formik.values.already} className="CheckBox" />
+                </div>  
+        </div>
+    }
+
+    return component;
+}
+
+export default FormGroupeBottom;
